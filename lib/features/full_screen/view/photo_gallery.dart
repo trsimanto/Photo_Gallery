3d7d368563a075c6dc9d6ee8a7b97bb14b3f5d07
trsimanto <simanto14feb@gmail.com>
2023-02-14 16:45:45 +0600

    PhotoFullScreen created

---
+++ lib/features/full_screen/view/photo_gallery.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class PhotoFullScreen extends StatelessWidget {
+  const PhotoFullScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: const Text("Photo FullScreen"),
+      ),
+    );
+  }
+}
