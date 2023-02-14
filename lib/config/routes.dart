62fcfcad801322e5f149486319b40e5f9046ec5d
trsimanto <simanto14feb@gmail.com>
2023-02-14 16:28:29 +0600

    Common widget added

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,3 +1,4 @@
 class PhotoGalleryRoutes {
   static const String photoGallery = "/photoGallery";
+  static const String photoFullscreen = "/photoFullscreen";
 }
---
+++ lib/widget/common_widget.dart
@@ -0,0 +1,52 @@
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pinch_zoom/pinch_zoom.dart';
+
+import '../config/routes.dart';
+
+PGImage(String imageUrl, {bool isClickable = true}) {
+  return
+    isClickable?
+    CachedNetworkImage(
+      imageUrl: imageUrl,
+      imageBuilder: (context, imageProvider) =>
+          InkWell(
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                PhotoGalleryRoutes.photoFullscreen,
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+      errorWidget: (context, url, error) => Container(
+        color: Colors.transparent,
+      ),
+    ):
+    PinchZoom(
+    resetDuration: const Duration(milliseconds: 100),
+    maxScale: 2.5,
+    child:  CachedNetworkImage(
+    imageUrl: imageUrl,
+    imageBuilder: (context, imageProvider) =>
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: imageProvider,
+            ),
+          ),
+        ),
+    errorWidget: (context, url, error) => Container(
+      color: Colors.transparent,
+    ),
+  ));
+}
