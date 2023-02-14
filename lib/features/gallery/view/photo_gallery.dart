bdf16e7aaefd9a65ec334cee0f2a607955d314e0
trsimanto <simanto14feb@gmail.com>
2023-02-14 12:52:58 +0600

    PhotoGallery init

---
+++ lib/features/gallery/view/photo_gallery.dart
@@ -0,0 +1,9 @@
+
+class PhotoGallery extends StatelessWidget {
+  const PhotoGallery({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Container();
+  }
+}
